858dbc1079e15f211ccae5a779f017aeb48af957
takeda <takeda.hrk@gmail.com>
2015-06-09 21:28:17 +0900

    コメント＆ちょこっとリファクタリング

--- backupDropBox.lua
+++ backupDropBox.lua
@@ -1,20 +1,21 @@
-local folder = "/Upload" 
-local file = "IMGP0911.JPG"
-local file_path = folder .. "/" .. file
-  --get the size of the file
+folder = "/Upload" 
+file = "IMGP0911.JPG"
+access_token = "**dropBoxAccessToken**"
+file_path = folder .. "/" .. file
+  --ファイルサイズ取得
   local filesize = lfs.attributes(file_path,"size")
-  if filesize ~= nil then
-  else
+  if filesize == nil then
     return
   end
-  --Upload!
+  
+  --dropBoxへアップロード
   b, c, h = fa.request{
-    url = "https://api-content.dropbox.com/1/files_put/dropbox/"..file.."?overwrite=true",
+    url = "https://api-content.dropbox.com/1/files_put/dropbox/" .. file .. "?overwrite=true",
     method = "PUT",
-    headers = {["Authorization"] = "Bearer token",
+    headers = {["Authorization"] = "Bearer " .. access_token,
     ["Content-Length"] = filesize},
-    file=file_path,
-    bufsize=1460*10
+    file = file_path,
+    bufsize = 1460*10
   }
 print(c)
 print(b)
